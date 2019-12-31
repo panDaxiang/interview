35133ebd4f443a4da3d597647df2beb945edf9c7
panxiang <734191973@qq.com>
2019-12-31 21:05:23 +0800

    fix: 修改数组去重逻辑

--- js/1.数组内对象工具属性值去重.js
+++ js/1.数组内对象工具属性值去重.js
@@ -1,16 +1,22 @@
 let arr = [
   { name: "小王", age: 16 },
   { name: "小王吧", age: 14 },
-  { name: "小王", age: 19 }
-]; 
+  { name: "小王", age: 19 },
+  { name: 1, age: 10 },
+  { name: "1", age: 100 }
+];
 
-let obj = {};
-const newArr = arr.reduce((newArr, item) => {
-  if (!obj[item.name]) {
-    newArr.push(item);
-    obj[item.name] = true;
-  }
-  return newArr;
-}, []);
+function deleteRepaet(arr) {
+  let obj = new Map();
+  return arr.reduce((newArr, item) => {
+    if (!obj.has(item.name)) {
+      newArr.push(item);
+      obj.set(item.name, true);
+    }
+    return newArr;
+  }, []);
+}
 
-console.log(newArr);
+let result = deleteRepaet(arr);
+
+console.log(result);
