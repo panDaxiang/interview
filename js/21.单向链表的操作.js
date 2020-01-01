2daffeea2910cf27d7b15ea47424572fa0205ce3
panxiang <734191973@qq.com>
2020-01-01 13:13:40 +0800

    feat: 修改单链表的语法形式

--- js/21.单向链表的操作.js
+++ js/21.单向链表的操作.js
@@ -3,47 +3,42 @@ function Node(element) {
   this.next = null;
 }
 
-function LList() {
-  this.head = new Node("head");
-  this.find = find;
-  this.insert = insert;
-  this.display = display
-  this.remove = remove
-}
-
-function find(item) {
-  let currentNode = this.head;
-  while (item !== currentNode.element) {
-    currentNode = currentNode.next;
+class LList {
+  constructor() {
+    this.head = new Node("head");
   }
-  return currentNode;
-}
 
-function insert(newElement, item) {
-  let newNode = new Node(newElement);
-  let currentNode = this.find(item);
-  newNode.next = currentNode.next;
-  currentNode.next = newNode;
-}
-
-function display(){
-  let current = this.head
-  while(current.next !== null){
-    console.log(current.next.element);
-    current = current.next
+  find(item) {
+    let currentNode = this.head;
+    while (currentNode.element !== item) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+  insert(newElement, item) {
+    let currentNode = this.find(item);
+    let newNode = new Node(newElement);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+  }
+  display() {
+    let current = this.head;
+    while (current.next !== null) {
+      console.log(current.next.element);
+      current = current.next;
+    }
+  }
+  remove(item) {
+    let removeNode = this.find(item);
+    let nextNode = removeNode.next;
+    removeNode.element = nextNode.element;
+    removeNode.next = nextNode.next;
   }
 }
 
-function remove(item){
-  var currentNode = this.find(item)
-  let nextNode = currentNode.next
-  currentNode.element = nextNode.element
-  currentNode.next = nextNode.next
-}
-
-var fruits = new LList();
-fruits.insert('apple', 'head')
-fruits.insert('pear', 'apple')
-fruits.insert('banana', 'pear')
-fruits.remove('apple')
-fruits.display()
+const list = new LList();
+list.insert("apple", "head");
+list.insert("banana", "apple");
+list.insert("pear", "banana");
+list.remove("apple");
+list.display();
