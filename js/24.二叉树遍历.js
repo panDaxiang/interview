2b5254dc7d210b02eb64a8aa410baca036e03561
panxiang <734191973@qq.com>
2020-01-01 21:18:49 +0800

    feat: 二叉树广度优先遍历

--- js/24.二叉树遍历.js
+++ js/24.二叉树遍历.js
@@ -1,25 +1,24 @@
-function TreeNode(element){
-  this.element = element
-  this.left = null
-  this.right  = null
+function TreeNode(element) {
+  this.element = element;
+  this.left = null;
+  this.right = null;
 }
 
-class Tree{
-  createTree(){
-    let tree = new TreeNode('A')
-    tree.left = new TreeNode('B')
-    tree.left.left = new TreeNode('D')
-    tree.left.left.left = new TreeNode('E')
-    tree.left.right = new TreeNode('F')
-    tree.left.right.left = new TreeNode('G')
-    tree.right = new TreeNode('C')
-    return tree
+class Tree {
+  createTree() {
+    let tree = new TreeNode("A");
+    tree.left = new TreeNode("B");
+    tree.left.left = new TreeNode("D");
+    tree.left.left.left = new TreeNode("E");
+    tree.left.right = new TreeNode("F");
+    tree.left.right.left = new TreeNode("G");
+    tree.right = new TreeNode("C");
+    return tree;
   }
-
 }
 
-let tree = new Tree()
-let treeNode = tree.createTree()
+let tree = new Tree();
+let treeNode = tree.createTree();
 
 // 前序遍历 根-左-右
 function DLR(obj) {
@@ -31,39 +30,52 @@ function DLR(obj) {
     DLR(obj.right);
   }
 }
-console.log('前序遍历:');
-DLR(treeNode); 
+console.log("前序遍历:");
+DLR(treeNode);
 
 // 中序遍历 左-根-右
-function LDR(obj){
-  if(obj.left){
-    LDR(obj.left)
+function LDR(obj) {
+  if (obj.left) {
+    LDR(obj.left);
   }
   console.log(obj.element);
-  if(obj.right){
-    LDR(obj.right)
+  if (obj.right) {
+    LDR(obj.right);
   }
 }
-console.log('中序遍历:');
-LDR(treeNode)
+console.log("中序遍历:");
+LDR(treeNode);
 
 // 后序遍历 左-右-根
-function LRD(obj){
-  if(obj.left){
-    LRD(obj.left)
+function LRD(obj) {
+  if (obj.left) {
+    LRD(obj.left);
   }
-  if(obj.right){
-    LRD(obj.right)
+  if (obj.right) {
+    LRD(obj.right);
   }
   console.log(obj.element);
 }
-console.log('后序遍历:');
-LRD(treeNode)
+console.log("后序遍历:");
+LRD(treeNode);
 
-// 深度优先遍历
-function levelTraversal(obj){
-  
+// 广度优先遍历
+function levelTraversal(obj) {
+  let result = [];
+  let tree = [];
+  tree.push(obj);
+  while (tree.length) {
+    var node = tree.shift();
+    result.push(node.element);
+    if (node.left) {
+      tree.push(node.left);
+    }
+
+    if (node.right) {
+      tree.push(node.right);
+    }
+  }
+  console.log(result);
 }
 
-
-
+levelTraversal(treeNode);
