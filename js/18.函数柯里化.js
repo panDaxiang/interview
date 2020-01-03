0390637af8d345f135d97f9ecbc9424f1f9b48ce
panxiang <734191973@qq.com>
2020-01-03 21:24:09 +0800

    feat: 函数柯里化

--- js/18.函数柯里化.js
+++ js/18.函数柯里化.js
@@ -15,3 +15,18 @@ function add() {
 let result = add(1, 2, 3)();
 let result2 = add(1)(2)(3)();
 console.log(result, result2);
+
+// 写一个加法函数(sum)，使他可以同时支持sum(x,y)和sum(x)(y)两种调用方式 
+function sum(){
+  let args = [...arguments]
+  if(args.length == 2){
+    return args.reduce((a, b) => a + b)
+  }
+
+  return function(){
+    args = args.concat([...arguments])
+    return sum(...args)
+  }
+}
+
+console.log(sum(1)(2))
